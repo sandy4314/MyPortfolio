b3319ee2760a7b607c15d6dddd193a122a60948c
sandy4314 <sandeepkumaronteru7777@gmail.com>
2025-02-03 23:11:06 +0530

    updated

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
-  
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 }).then(() => console.log("MongoDB Connected"))
   .catch(err => console.error(err));
 
